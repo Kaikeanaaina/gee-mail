4ea857a1553e377825fbbbb47ae06e239248aea9
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-09-30 19:11:09 -1000

    added a mouse outs and mouse overs for the sub_mes classes.

--- js/my.js
+++ js/my.js
@@ -1,12 +1,49 @@
 window.onload = function(){
 	
-	var message = document.getElementsByClassName('message');
-		message.addEventListener('mouseover',function(){
+
+	var main_mes = document.getElementsByClassName('main_mes');
+		//this will change the background color of a message when it's mouseover
+		main_mes.addEventListener('mouseover',function(){
 			this.style.backgroundColor = '#FFA1A1';
 		})
-		
-		message.addEventListener('mouseout',function(){
+		//this will change back the message to the regular color when mouseout
+		main_mes.addEventListener('mouseout',function(){
 			this.style.backgroundColor = 'inherit';
 		})
-		
+
+
+		//this will call on the sub_mes classes
+	var messageStuff = document.getElementsByClassName('sub_mes')[0];
+	
+	//this will hide the sub_mes. by setting display to none
+	messageStuff.querySelectorAll('ul')[0].style.display = 'none';
+
+	//when click it will display the sub_mes by setting display to block
+	messageStuff.addEventListener('click', function(){
+		//show the sub_mes
+		this.querySelectorAll('ul')[0].style.display = 'block';
+
+	});
+
+	//when mouseout out of a sub_mes it will dissapear from display
+	//**** QUESTION ***** is mouseout the right term to use for something
+	//that has been click to return to it's normal state?
+	messageStuff.addEventListener('mouseout', function(){
+			this.querySelectorAll('ul')[0].style.display = 'none';
+		});
+
+	//now i want the sub_mes to be highlighted when mouseover
+	//this is after the message is open
+	messageStuff.addEventListener('mouseover',function(){
+		this.style.backgroundColor = 'FFA1A1';
+	})
+	//returning it back to inherit
+	messageStuff.addEventListener('mouseout',function(){
+		this.style.backgroundColor = 'inherit';
+	})
+
+	
+
+
+
 };
