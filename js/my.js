6d486f98a2b637a3d69382819a119879c235ebd2
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-10-01 02:55:58 -1000

    assigned id in the html, mouover and mouse out in js for sub_mes class

--- js/my.js
+++ js/my.js
@@ -15,29 +15,29 @@ window.onload = function(){
 		//this will call on the sub_mes classes
 	var messageStuff = document.getElementsByClassName('sub_mes')[0];
 	
-	//this will hide the sub_mes. by setting display to none
+	// hide the sub_mes
 	messageStuff.querySelectorAll('ul')[0].style.display = 'none';
 
-	//when click it will display the sub_mes by setting display to block
+	//click it
 	messageStuff.addEventListener('click', function(){
 		//show the sub_mes
 		this.querySelectorAll('ul')[0].style.display = 'block';
 
 	});
 
-	//when mouseout out of a sub_mes it will dissapear from display
+	//mouseout out of a sub_mes it will dissapear from display
 	//**** QUESTION ***** is mouseout the right term to use for something
 	//that has been click to return to it's normal state?
 	messageStuff.addEventListener('mouseout', function(){
 			this.querySelectorAll('ul')[0].style.display = 'none';
 		});
 
-	//now i want the sub_mes to be highlighted when mouseover
-	//this is after the message is open
+	//highlighted sub_mes when mouseover
+	//this is after the message is clicked open
 	messageStuff.addEventListener('mouseover',function(){
 		this.style.backgroundColor = 'FFA1A1';
 	})
-	//returning it back to inherit
+	//return inherit when mouseout
 	messageStuff.addEventListener('mouseout',function(){
 		this.style.backgroundColor = 'inherit';
 	})
