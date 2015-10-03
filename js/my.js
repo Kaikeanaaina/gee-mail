ef746e8f072571729faa2592f9b8a976356d39a8
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-10-02 18:50:56 -1000

    refreshed my index to default

--- js/my.js
+++ js/my.js
@@ -1,67 +1,39 @@
 window.onload = function(){
 	
-	//need to find a way to count the emails and display it
-	//i assume we will be counting an array of messages
-	//there will need to be a count that can keep up with the input
-	var counter = document.getElementById('counter');
-	counter.InnerHTML=messageCount;
-	document.body.appendChild(counter);
+	//message count
+	function messageCount(messageArray){
+		console.log(messageArray.length);
+		}
+	}
 
-	//below here is an example of the array of messages
-	//i used strings for now
-	var count = ['kai','noa','people','love'];
-	//i used a plus one in the for loop 
-	//because the array counts from zero
-	var messageCount = function(arrMessage){
-		for(var i=0;i< arrMessage.length;i++){
-			console.log((i)+1);
+	//Listing messages and the context in it
+	var messageList= function(messageArray){
+		for(var i=0;i<messageArray.length;i++){
+			console.log(messageArray(i));
 		}
 	};
-	//called the function
-	messageCount(count);
 
+	for(var i=0;i<messageArray.length;i++){
+		//this will allow us 
+		//to access the properties of the array within the for loop
+		var message[i]= {
+			date: date[i];
+			subject: subject[i];
+			sender: sender[i];
+			body: body[i];
+		};
 
-	var main_mes = document.getElementById('main_mes');
-		//this will change the background color of a message when it's mouseover
-	main_mes.addEventListener('mouseover',function(){
-		this.style.backgroundColor = '#FFA1A1';
-		this.style.color = '#0033FF';
-	});
-	//this will change back the message to the regular color when mouseout
-	main_mes.addEventListener('mouseout',function(){
-		this.style.backgroundColor = 'inherit';
-		this.style.color = 'inherit';
-	});
-	
-	// hide the sub_mes
-	main_mes.querySelectorAll('ul')[0].style.display = 'none';
-	//click it
-	main_mes.addEventListener('click', function(){
-		//show the sub_mes
-		this.querySelectorAll('ul')[0].style.display = 'block';
+		var message = document.createElement('div');
+		message.className = message[i];
+		message.innerHTML = message[i].textContext;
+		document.body.appendChild('message');
 
-	});
-	
-	//this will call on the sub_mes classes
-	var messageStuff = document.getElementsByClassName('sub_mes')[0];
+		var ul = document.createElement('ul');
+		ul.Id = 'ul';
+		ul.innerHTML = message[i];
+		
 
-	//mouseout out of a sub_mes it will dissapear from display
-	//**** QUESTION ***** is mouseout the right term to use for something
-	//that has been click to return to it's normal state?
-	messageStuff.addEventListener('mouseout', function(){
-			this.querySelectorAll('ul')[0].style.display = 'none';
-		});
-	//highlighted sub_mes when mouseover
-	//this is after the message is clicked open
-	messageStuff.addEventListener('mouseover',function(){
-		this.style.backgroundColor = '#FFA1A1';
-	})
-	//return inherit when mouseout
-	messageStuff.addEventListener('mouseout',function(){
-		this.style.backgroundColor = 'inherit';
-	})
-
-
+	}
 
 
 
